3eb140d7ebddf9ffa9c3084f5b312c0d2844bd06
etan630 <79673596+etan630@users.noreply.github.com>
2024-08-27 16:01:01 -0400

    Create 80. Remove Duplicates from Sorted Array II.py

---
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        start = 1
+        i = 2
+
+        while i < len(nums):
+            #essentially you want to look for next i if there was already two numbers
+            if (nums[start] == nums[i] and nums[start - 1] == nums[start]):
+                i += 1
+            else:
+                start += 1
+                nums[start] = nums[i]
+                i += 1
+
+        return start + 1
