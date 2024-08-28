677db85ece5b2df18dd8394391a6a938e70fcd4c
etan630 <79673596+etan630@users.noreply.github.com>
2024-08-28 16:42:06 -0400

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -10,26 +10,26 @@ class Solution(object):
 
         # since each element appears more than n/2 time you 
         # could just find the element there
-        # return nums[len(nums) // 2]
+        return nums[len(nums) // 2]
 
         #alternative solution
 
-        start = 0
-        i = 0
-        count = 0
-        k = nums[0]
+        #start = 0
+        #i = 0
+        #count = 0
+        #k = nums[0]
 
         # iterate i throughout the array
-        while i < len(nums):
+        #while i < len(nums):
             # count the number of times you would have to iterate
             # store current most occurance
-            if nums[start] == nums[i]:
-                i += 1
-                count += 1
-                k = nums[start]
+            #if nums[start] == nums[i]:
+                #i += 1
+                #count += 1
+                #k = nums[start]
             
             # move start to current position of i to check next
-            start = i
-            i += count
+            #start = i
+            #i += count
 
-        return k
+        #return k
